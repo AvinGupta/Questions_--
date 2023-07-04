2520570039e97d2ea5602f1b0780fd6f6dcf8a22
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-05 04:05:44 +0530

    Update and rename Binary Search to Binary Search.java

--- Binary_Search_--/Binary Search.java
+++ Binary_Search_--/Binary Search.java
@@ -1,4 +1,5 @@
 class Solution {
+    // recusive solution
     int binarysearch(int arr[], int n, int k) {
         return bin(arr,k,0,arr.length-1);
     }
@@ -8,5 +9,18 @@ class Solution {
         if(arr[mid]==k) return mid;
         if(arr[mid]<k) return bin(arr,k,mid+1,ept);
         return bin(arr,k,spt,mid-1);
+    }
+}
+class Solution {
+    // iterative solution
+    int binarysearch(int arr[], int n, int k) {
+        int spt=0,ept=arr.length-1;
+        while(spt<=ept){
+            int mid=spt+(ept-spt)/2;
+            if(arr[mid]==k) return mid;
+            else if(arr[mid]>k) ept=mid-1;
+            else spt=mid+1;
+        }
+        return -1;
     }
 }
