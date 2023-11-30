b9add4b0de4e23c0016904a7a4ab414a005c16f1
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-11-30 17:50:58 +0530

    Create Shortest Common Supersequence.java

---
+++ Dynamic_Programming_--/Shortest Common Supersequence.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String x,String y,int m,int n)
+    {
+        //Your code here
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++) dp[i][0]=0;
+        for(int i=0;i<n+1;i++) dp[0][i]=0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(x.charAt(i-1)==y.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return x.length()+y.length()-dp[m][n];
+    }
+}
