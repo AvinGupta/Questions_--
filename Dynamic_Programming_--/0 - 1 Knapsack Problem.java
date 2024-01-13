148f3f75745466ee5aeb42b2dfc4dbc4e61606d3
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-01-13 21:45:36 +0530

    Update 0 - 1 Knapsack Problem.java

--- Dynamic_Programming_--/0 - 1 Knapsack Problem.java
+++ Dynamic_Programming_--/0 - 1 Knapsack Problem.java
@@ -3,7 +3,7 @@ class Solution
     //top-down dp.
     //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int w, int wt[], int val[], int n) 
-    { 
+    {  
         // your code here
         int[][] dp=new int[n+1][w+1];
         for(int i=0;i<n+1;i++){
