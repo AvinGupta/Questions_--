d7a68cda1905797048ce2aaf16554c19b40a25c5
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-01-14 19:02:19 +0530

    Update Partition Equal Subset Sum.java

--- Dynamic_Programming_--/Partition Equal Subset Sum.java
+++ Dynamic_Programming_--/Partition Equal Subset Sum.java
@@ -1,24 +1,29 @@
-class Solution{
-    static int equalPartition(int N, int arr[])
-    {
-        // code here
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-        if(sum%2!=0) return 0;
-        return subsetSum(arr,sum/2,N);
-    }
-    static int subsetSum(int[] arr,int sum,int n){
-        int[][] dp=new int[n+1][sum+1];
-        for(int i=0;i<sum+1;i++) dp[0][i]=0;
-        for(int i=0;i<n+1;i++) dp[i][0]=1;
+public class Solution {
+	public static boolean canPartition(int[] arr, int n) {
+		// Write your code here.
+		int sum=0;
+		for(int var:arr) sum+=var;
+		if(sum%2!=0) return false;
+        return subsetSumToK(n,sum/2,arr);
+	}
+	public static boolean subsetSumToK(int n, int w, int arr[]){
+        // Write your code here.
+        boolean[][] dp=new boolean[n+1][w+1];
+        //memoization
+        // return fnc(arr,n,w,dp); 
+        //tabulation
+        for(int i=0;i<w+1;i++) dp[0][i]=false;
+        for(int i=0;i<n+1;i++) dp[i][0]=true;
         for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if(arr[i-1]<=j) dp[i][j]=Math.max(dp[i-1][j-arr[i-1]],dp[i-1][j]);
-                else dp[i][j]=dp[i-1][j];
+            for(int j=1;j<w+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else if(arr[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
             }
         }
-        return dp[n][sum];
+        return dp[n][w];
     }
 }
