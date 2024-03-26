bd29c5d1a05b5a80daaa18acfca83d03bbf18d70
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-03-27 04:04:20 +0530

    Create Delete Kth Node From End.java

---
+++ LinkedList_--/Delete Kth Node From End.java
@@ -0,0 +1,52 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node removeKthNode(Node head, int K)
+    {
+        // Write your code here.
+        Node fast=head,slow=head;
+        for(int i=0;i<K;i++) fast=fast.next;
+        if(fast==null){
+            head=head.next;
+            return head;
+        }
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
