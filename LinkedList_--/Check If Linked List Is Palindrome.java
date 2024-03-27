43a9acc11578dba789197dba12189c7220d7a7c2
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-03-28 01:32:48 +0530

    Create Check If Linked List Is Palindrome.java

---
+++ LinkedList_--/Check If Linked List Is Palindrome.java
@@ -0,0 +1,65 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static Node reverse(Node head){
+        if(head==null || head.next==null) return head;
+        Node curr=head;
+        Node prev=null;
+        while(curr!=null){
+            Node temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    public static boolean isPalindrome(Node head) {
+        // write your code here
+        Node fast=head;
+        Node slow=head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        Node newhead=reverse(slow.next);
+        Node first=head;
+        Node second=newhead;
+        while(second!=null){
+            if(first.data!=second.data){
+                reverse(newhead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        reverse(newhead);
+        return  true;
+    }
+}
