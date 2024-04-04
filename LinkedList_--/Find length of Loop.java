87546e04bcf8f067bd4d1c602353d5057c692d93
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-04-04 19:48:30 +0530

    Create Find length of Loop.java

---
+++ LinkedList_--/Find length of Loop.java
@@ -0,0 +1,51 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static int lengthOfLoop(Node head) {
+        // Write your code here
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                int cnt=1;
+                fast=fast.next;
+                while(fast!=slow){
+                    cnt++;
+                    fast=fast.next;
+                }
+                return cnt;
+            }
+        }
+        return 0;
+    }
+}
