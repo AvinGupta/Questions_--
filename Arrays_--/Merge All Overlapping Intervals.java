8f9d622fe04f4de87a5e01d63289b7491d7dc824
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-05-06 22:21:48 +0530

    Create Merge All Overlapping Intervals.java

---
+++ Arrays_--/Merge All Overlapping Intervals.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+    public static List< List< Integer > > mergeOverlappingIntervals(int [][]arr){
+        // Write your code here.
+        List<List<Integer>> lst=new ArrayList<>();
+        if(arr.length==0 || arr==null){
+            lst.add(new ArrayList<>());
+            return lst;
+        } 
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int start=arr[0][0];
+        int end=arr[0][1];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i][0]<=end){
+                end=Math.max(end,arr[i][1]);
+            }
+            else{
+                lst.add(Arrays.asList(start,end));
+                start=arr[i][0];
+                end=arr[i][1];
+            }
+        }
+        lst.add(Arrays.asList(start,end));
+        return lst;
+    }
+}
