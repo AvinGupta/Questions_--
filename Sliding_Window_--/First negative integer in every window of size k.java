f80ff15e634397999e949cc798c8a296516105ea
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-05-24 10:53:05 +0530

    Create First negative integer in every window of size k.java

--- Sliding_Window_--/First negative integer in every window of size k.java
+++ Sliding_Window_--/First negative integer in every window of size k.java
@@ -1,13 +1,15 @@
-int spt=0;
+class Compute {
+    public long[] printFirstNegativeInteger(long arr[], int N, int K){
+        int spt=0;
         int ept=0;
-        int[] ans=new int[N-K+1];///answer of size N-K+1 where n is the size of array and k is window size
-        ArrayList<Integer> lst=new ArrayList<>();
-        ArrayList<Integer> lst1=new ArrayList<>();
+        long[] ans=new long[N-K+1];// here n-k+1 is the number of windows of size k in array of length n.
+        ArrayList<Long> lst=new ArrayList<>();
+        ArrayList<Long> lst1=new ArrayList<>();
         while(ept<N){
             if(arr[ept]<0) lst.add(arr[ept]);
             if(ept-spt+1<K)ept++;
             else if(ept-spt+1==K){
-                if(lst.size()==0) lst1.add(0);
+                if(lst.size()==0) lst1.add((long)0);
                 else {
                     lst1.add(lst.get(0));
                     if(arr[spt]<0) lst.remove(0);
@@ -15,9 +17,10 @@ int spt=0;
                 spt++;
                 ept++;
             }
-        }
-        // converting list into array to match the return type 
+        }//converting the list into the array for output
         for(int i=0;i< ans.length;i++){
             ans[i]=lst1.get(i);
         }
         return ans;
+    }
+}
