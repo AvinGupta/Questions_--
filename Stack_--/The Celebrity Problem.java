eaa5d988852f2d39ef73b6bebb9d981ab8287efa
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-08-08 14:58:30 +0530

    Create The Celebrity Problem.java

---
+++ Stack_--/The Celebrity Problem.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Function to find if there is a celebrity in the party or not.
+    public int celebrity(int mat[][]) {
+        // code here
+        int top=0;
+        int down=mat.length-1;
+        while(top<down){
+            if(mat[top][down]==1) top++;
+            else if(mat[down][top]==1) down--;
+            else{
+                top++;
+                down--;
+            }
+        }
+        if(top>down) return -1;
+        for(int i=0;i<mat.length;i++){
+            if(i==top) continue;
+            if(mat[top][i]==0 && mat[i][top]==1){
+                
+            }
+            else return -1;
+        }
+        return top;
+    }
+}
