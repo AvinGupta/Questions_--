53be71f28dee5a148bc7b0efc7958f97bb99c34a
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2024-08-11 21:54:21 +0530

    Create Postfix to Infix Conversion.java

---
+++ Stack_--/Postfix to Infix Conversion.java
@@ -0,0 +1,21 @@
+class Solution {
+    static String postToInfix(String exp) {
+        // code here
+        int i=0;
+        Stack<String> st=new Stack<>();
+        while(i<exp.length()){
+            char ch=exp.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+                st.push(ch+"");
+            }
+            else{
+                String x=st.pop();
+                String y=st.pop();
+                String ans='('+y+ch+x+')';
+                st.push(ans);
+            }
+            i++;
+        }
+        return st.peek();
+    }
+}
